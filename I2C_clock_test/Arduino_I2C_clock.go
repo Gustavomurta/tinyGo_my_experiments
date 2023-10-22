b5894aff91283c8e7db2a5133d24197579566870
Jose Gustavo Abreu Murta <jgustavoam@gmail.com>
2023-10-22 14:21:55 -0300

    Create Arduino_I2C_clock.go

---
+++ I2C_clock_test/Arduino_I2C_clock.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"device/avr"
+	"machine"
+)
+
+var (
+	i2cFrequency uint32 = 100000
+	dataTest            = []byte{0x57, 0x00, 0x01, 0x2, 0x03, 0x04}
+)
+
+func main() {
+
+	avr.TWSR.ClearBits((avr.TWSR_TWPS0 | avr.TWSR_TWPS1))
+	avr.TWBR.Set(uint8(((machine.CPUFrequency() / i2cFrequency) - 16) / 2))
+	avr.TWCR.Set(avr.TWCR_TWEN)
+
+	// Clear TWI interrupt flag, put start condition on SDA, and enable TWI.
+	avr.TWCR.Set((avr.TWCR_TWINT | avr.TWCR_TWSTA | avr.TWCR_TWEN))
+	//avr.TWCR.Set(avr.TWCR_TWINT)
+
+	for i := 0; i < 7; i++ {
+		writeByte(dataTest[i])
+	}
+}
+
+// writeByte writes a single byte to the I2C bus.
+func writeByte(data byte) error {
+	// Write data to register.
+	avr.TWDR.Set(data)
+
+	// Clear TWI interrupt flag and enable TWI.
+	avr.TWCR.Set(avr.TWCR_TWEN | avr.TWCR_TWINT)
+
+	// Wait till data is transmitted.
+	for !avr.TWCR.HasBits(avr.TWCR_TWINT) {
+	}
+	return nil
+
+}
