8c04b3823b638b61e5061375f39b7055085ce66b
Jose Gustavo Abreu Murta <jgustavoam@gmail.com>
2025-09-16 01:24:35 -0300

    Update main.go

--- Raspberry_Pico/DVM_4halfDigits/tinygo/main.go
+++ Raspberry_Pico/DVM_4halfDigits/tinygo/main.go
@@ -1,19 +1,17 @@
 /*
-Dual Slope ADC Example for Raspberry Pi Pico - RP2040 - Version 9
-Gustavo Murta 2025_09_09 - MIT license
-tinygo version 0.38.0 windows/amd64 (using go version go1.24.2 and LLVM version 19.1.2)
+Dual Slope ADC Example for Raspberry Pi Pico - RP2040 - Version 10
+Gustavo Murta 2025_09_15
+tinygo version 0.39.0 windows/amd64 (using go version go1.25.1 and LLVM version 19.1.2)
 C:\Users\jgust\tinygo\programas\Raspberry_Pico\Dual_Slope_ADC_Pico
 tinygo flash -target pico main.go
 
-https://github.com/tinygo-org/tinygo/tree/release/src/examples/pininterrupt
-
 74HC4051 S0	= GPIO10
 74HC4051 S1	= GPIO11
-Comparator Q1 VOUT = GPIO15 (interrupt input pin)
+VOUT Q1	= GPIO15  (interrupt pin) - Comparator Output
 
-A4 - GND at input
-A5 - V input
-A6 - V Reference -2.500 V
+A4 - GND input - Auto Zero
+A5 - V input integration
+A6 - V Reference -2.500 V integration
 A7 - Reset Integrator
 */
 
@@ -24,34 +22,26 @@ import (
 	"fmt"
 	"machine"
 	"time"
-	//"tinygo.org/x/tinygo/src/machine"	
+	//"tinygo.org/x/tinygo/src/machine"
 )
 
 var (
-	mux_S0        = machine.GPIO10
-	mux_S1        = machine.GPIO11
-	comparatorPin = machine.GPIO15
-	tpA           = machine.GPIO13
+	mux_S0        = machine.GPIO10 // mux adress bit 0
+	mux_S1        = machine.GPIO11 // mux adress bit 1
+	comparatorPin = machine.GPIO15 // Comparator Output - Interrupt Pin
+	tpA           = machine.GPIO13 // Test Point A
 
-	startCounter1, endCounter1 = uint32(0), uint32(0)
-	counter1                   = uint32(0)
-	startCounter2, endCounter2 = uint32(0), uint32(0)
-	counter2                   = uint32(0)
+	startPeriod, endPeriod = uint32(0), uint32(0)
 
-	startPeriod, startPeriodVIN = uint32(0), uint32(0)
-	endPeriod, endPeriodVIN     = uint32(0), uint32(0)
-	periodVIN                   = uint32(0)
-	width                       = uint32(0)
-	widthnS                     = uint32(0)
-	widthZero                   = uint32(0)
+	autoZeroPeriod = uint32(0)
 
-	voltageRead            = false
-	vinPeriod, vinPeriodns = uint32(0), uint32(0)
-	//startTime              = time.Now() // Start measuring time
-	voltage = float32(0)
+	voltageRead                = false
+	vrefPeriod, vrefPeriodTime = uint32(0), uint32(0)
+	vinPeriod, vinPeriodTime   = uint32(0), uint32(0)
+	voltage, voltageZero       = float32(0), float32(0)
 )
 
-const MAX_RELOAD = 0xF42400 // Maximum reload value (24 bits): 16,777,216 ticks 0xFFFFFF
+const MAX_RELOAD = 0xFFFFFF // Maximum reload value (24 bits): 16,777,216 ticks 0xFFFFFF
 
 // Enable SysTick counter
 func enableSysTick() {
@@ -66,65 +56,62 @@ func readSysTick() uint32 {
 	return rp.PPB.GetSYST_CVR_CURRENT() // Read current SysTick value (24 bits)
 }
 
-// Delay with Systick Timer - processor clock
-func delayMSsystick(ticks uint32) {
+// Delay with Systick Timer - processor clock 200 MHz - max: 83 ms
+func delaySystick(ticks uint32) {
 	startSystick := readSysTick()  // Read current systick timer
 	target := startSystick - ticks // Calculate target delay
 	for readSysTick() > target {   // Wait target timeout
 	}
 }
 
-func testPointA() {
-	tpA.High()         // Set GPIO13
-	delayMSsystick(10) // 10 sticks = 50 ns
-	tpA.Low()          // Reset GPIO13
+func testPointA() { // used only for testing with logic analyzer
+	tpA.High()      // Set GPIO13 High
+	delaySystick(5) // 5 ticks = 25 ns
+	tpA.Low()       // Set GPIO13 Low
 }
 
-// Phase I - Reset Integrator - T0
+// Phase I - Reset Integrator
 // 74HC4051 - set Address 7 => Reset Integrator
 func resetIntegrator() {
-	mux_S0.High()                   // MUX bit1
-	mux_S1.High()                   // MUX bit2
-	delayMSsystick((5e6 / 5) - 125) // Delay to Capacitor discharge 5 ms = ((5e6 / 5) - 125)
+	mux_S0.High()            // MUX bit0
+	mux_S1.High()            // MUX bit1
+	delaySystick((10e6 / 5)) // Delay to Capacitor discharge 5 ms = ((5e6 / 5) - 125)
 }
 
-// Phase II - Integrate V Input - T1
+// Phase II - Integrate V Input
 // 74HC4051 - set Address 5 => Read VIN
 func integVIN() {
-	mux_S0.High()                   // MUX bit1
-	mux_S1.Low()                    // MUX bit2
-	startPeriodVIN = readSysTick()  // Read SysTick before start integration
-	delayMSsystick((10e6 / 5) - 30) // Read VIN 10 ms = ((10e6 / 5) - 240)
-	endPeriodVIN = readSysTick()    // Read SysTick after end integration
+	mux_S0.High()                  // MUX bit0
+	mux_S1.Low()                   // MUX bit1
+	delaySystick((10e6 / 5) - 240) // Read VIN 10 ms = ((10e6 / 5) - 240)
 }
 
-// Phase III - Integrate - VREF - T2
+// Phase III - Integrate - VREF
 // 74HC4051 - set Address 6 => Read -VREF
 func integVREF() {
-	mux_S0.Low()  // MUX bit1
-	mux_S1.High() // MUX bit2
-	//testPointA()              // Set GPIO13
+	mux_S0.Low()                // MUX bit0
+	mux_S1.High()               // MUX bit1
 	startPeriod = readSysTick() // Read SysTick before start integration
-	delayMSsystick((15e6 / 5))  // Read VREF 6 ms = 6e6 ns ((6e6 / 5) - 18)
+	delaySystick((11e6 / 5))    // Read VREF 11 ms - VREF time must be bigger than VIN time
 }
 
-// Phase IV - GND at Input
+// Phase IV - GND at Input - Auto Zero
 // 74HC4051 - set Address 4
 func gndInput() {
-	mux_S0.Low()                     // MUX bit1
-	mux_S1.Low()                     // MUX bit2
-	delayMSsystick((10e6 / 5) - 230) // Read VIN 10 ms = ((10e6 / 5) - 240)
+	mux_S0.Low()           // MUX bit0
+	mux_S1.Low()           // MUX bit1
+	delaySystick(10e6 / 5) // auto Zero at VIN 10 ms = minimum time for zeroing
 }
 
-func zeroADC() {
-	rp.PPB.SetSYST_CVR_CURRENT(0) // Clear current value (24 bits) SysTick timer - max time 83 ns
+func autoZeroADC() { // read Zero Volts value
+	rp.PPB.SetSYST_CVR_CURRENT(0) // Clear current value (24 bits)
 	resetIntegrator()             // Reset Integrator
-	gndInput()                    // zero Integrator
+	gndInput()                    // auto Zero Integrator
 	integVREF()                   // Integrate V REF
 }
 
 func dualSlopeADC() {
-	rp.PPB.SetSYST_CVR_CURRENT(0) // Clear current value (24 bits) SysTick timer - max time 83 ns
+	rp.PPB.SetSYST_CVR_CURRENT(0) // Clear current value (24 bits)
 	resetIntegrator()             // Reset Integrator
 	integVIN()                    // Integrate V Input
 	integVREF()                   // Integrate V REF
@@ -132,7 +119,6 @@ func dualSlopeADC() {
 
 func adcComparatorISR(pin machine.Pin) {
 	endPeriod = readSysTick() // Read SysTick after rising interrupt
-	//width = startPeriod - endPeriod
 	voltageRead = true
 }
 
@@ -140,39 +126,35 @@ func main() {
 
 	machine.InitSerial()        // Initialize serial for debug output
 	time.Sleep(3 * time.Second) // Sleep to catch prints on serial
-	fmt.Printf("Dual Slope ADC RP2040 V9 \n")
+	fmt.Printf("Dual Slope ADC RP2040 V10 \n")
 
-	// MUX Address pins configuration
+	// MUX pins configuration - as output
 	mux_S0.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	mux_S1.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	// Test Point A configuration 
-	tpA.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	// Test Point A configuration
+	tpA.Configure(machine.PinConfig{Mode: machine.PinOutput}) // Set GPIO13 as output
 
-	// Comparator interrupt PIN configuration
-	comparatorPin.Configure(machine.PinConfig{Mode: machine.PinInputPullup}) //PinInputPullup
-	comparatorPin.SetInterrupt(machine.PinRising, adcComparatorISR)
+	// Comparator pin configuration with interrupt
+	comparatorPin.Configure(machine.PinConfig{Mode: machine.PinInput}) // Set GPIO15 as input - interrupt pin
 
 	cpuFreq := machine.CPUFrequency() // Get CPU frequency
-	fmt.Println("CPU Frequency:", int(cpuFreq), "Hz")	
+	fmt.Println("CPU Frequency:", int(cpuFreq), "Hz")
 
-	nanoSECperTick := 1e9 / cpuFreq // Calculate nanoseconds per tick
-	fmt.Println("Nanoseconds per tick:", nanoSECperTick)
+	nanoSECperTicks := 1e9 / cpuFreq // Calculate nanoseconds per ticks
+	fmt.Println("Nanoseconds per tick:", nanoSECperTicks)
 
-	enableSysTick() // enable SysTick Timer
+	enableSysTick()                                                 // enable SysTick timer
+	comparatorPin.SetInterrupt(machine.PinRising, adcComparatorISR) // enable PinRising interrupt
 
 	for {
-
-		zeroADC()
+		autoZeroADC()
 
 		if voltageRead {
-			comparatorPin.SetInterrupt(machine.PinRising, nil) // Disable interrupt to avoid multiple triggers 
+			comparatorPin.SetInterrupt(machine.PinRising, nil) // Disable interrupt to avoid multiple triggers
 
-			widthZero = startPeriod - endPeriod
-			//fmt.Printf("Zero Width: %d ticks \n", widthZero)
-
-			//time.Sleep(time.Millisecond * 20)                               // delay 20 ms
-			delayMSsystick(20e6 / 5)
+			autoZeroPeriod = startPeriod - endPeriod                        // period of VIN = 0 Volts integration
+			time.Sleep(time.Millisecond * 20)                               // delay of 20 ms between measurements
 			comparatorPin.SetInterrupt(machine.PinRising, adcComparatorISR) // enable PinRising interrupt
 			voltageRead = false
 		}
@@ -180,36 +162,32 @@ func main() {
 		dualSlopeADC()
 
 		if voltageRead {
-			comparatorPin.SetInterrupt(machine.PinRising, nil) // Disable interrupt to avoid multiple triggers 
+			comparatorPin.SetInterrupt(machine.PinRising, nil) // Disable interrupt to avoid multiple triggers
 
-			width = startPeriod - endPeriod
-			widthnS = width * 5 // 5 ns per tick
-			periodVIN = startPeriodVIN - endPeriodVIN
+			vrefPeriod = startPeriod - endPeriod // period of VREF integration
+			//vrefPeriodTime = vrefPeriod * 5      // VREF Period time * 5 ns per tick
 
-			// fmt.Printf("startPeriod: %d ticks / ", startPeriod)
-			// fmt.Printf("endPeriod: %d ticks / ", endPeriod)
-			// width = startPeriod - periodWidth
+			fmt.Printf("Auto Zero Period: %d ticks // ", autoZeroPeriod)
+			voltageZero = float32(autoZeroPeriod) * 1.25e-6 // Auto zero voltage calculation
+			fmt.Printf("Voltage Auto Zero: %.3f V # ", voltageZero)
 
-			// fmt.Printf("Period VIN: %d ticks / ", periodVIN) //expected VIN period = 2e6 ticks
-			fmt.Printf("Width: %d ticks / ", width)
-			// fmt.Printf("Width ns: %d ns * ", widthnS)
+			fmt.Printf("VREF Period: %d ticks // ", vrefPeriod)
+			//fmt.Printf("VREF Period Time: %d ns # ", vrefPeriodTime)
 
-			// VREF / VIN period  = 2.500 / 2e6
-			vinPeriod = (width - widthZero)        // VREF Period width
-			vinPeriodns = vinPeriod * 5            // 5 ns per tick
-			voltage = float32(vinPeriod) * 1.25e-6 // Period VIN width * V bit 2.465e-6
+			// Calculation =>  VIN Voltage  = VIN period * (2.500 V / 2e6 ticks)
 
-			fmt.Printf("VIN Period: %d ticks / ", vinPeriod)
-			// fmt.Printf("VIN Period: %d ns * ", vinPeriodns)
-			fmt.Printf("Voltage: %.4f V\n", voltage)
-			//fmt.Printf("\n")
+			vinPeriod = (vrefPeriod - autoZeroPeriod) // period of VIN integration
+			vinPeriodTime = vinPeriod * 5             // VIN Period time * 5 ns per tick
+			voltage = float32(vinPeriod) * 1.25e-6    // VIN voltage calculation
+			fmt.Printf("VIN Voltage: %.3f V\n", voltage)
 
-			time.Sleep(time.Millisecond * 100)                              // delay 100 ms
+			time.Sleep(time.Millisecond * 125)                              // delay 125 ms => 5 measurements/second
 			comparatorPin.SetInterrupt(machine.PinRising, adcComparatorISR) // enable PinRising interrupt
 			voltageRead = false
 		}
 	}
 }
+
 
 
 
